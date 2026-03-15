09c01aedf2bd59ca0897d2b682873d03affbc093
naiksujan233-rgb <naiksujan233@gmail.com>
2026-03-15 22:34:50 +0530

    Update HELLO.java

--- HELLO.java
+++ HELLO.java
@@ -1,5 +1,5 @@
 public class HELLO {
     public static void main(String[] args) {
-        System.out.println("Hello srajans saye");
+        System.out.println("Hello srajansanju saye");
     }
 }
